668218df41772c3861824448052c6ae11f58238b
叶亮 <yeliang@tv365.net>
2019-12-28 20:54:57 +0800

    降低gradle版本，Androidx依赖改为supporty依赖，避免部分电脑gradle版本过低等编译不过问题。

--- app/src/main/java/com/play/ffmpeg/MainActivity.java
+++ app/src/main/java/com/play/ffmpeg/MainActivity.java
@@ -3,16 +3,16 @@ package com.play.ffmpeg;
 import android.Manifest;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.PermissionChecker;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
